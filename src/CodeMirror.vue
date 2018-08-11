d7aecf4d798e4707635a5ff1521e0232089864cb
Hal Lee <hal@hal-lee.com>
2018-08-11 10:52:24 -0400

    Mostly lint changes

--- src/CodeMirror.vue
+++ src/CodeMirror.vue
@@ -1,5 +1,5 @@
 <template>
-  <textarea></textarea>
+  <textarea />
 </template>
 
 <script>
@@ -8,20 +8,17 @@ const mode = { name: 'swift' }
 const CodeMirror = require('codemirror/lib/codemirror.js')
 require('codemirror/lib/codemirror.css')
 require(`../themes/${theme}.css`)
-// require('codemirror/addon/display/fullscreen.css')
-// require('codemirror/addon/display/fullscreen.js')
+
 export default {
-  data: function () {
-    return {
-      content: '',
-      editor: null,
-      keyMap: { 'Ctrl-Space': 'autocomplete'},
-      changesCounter: 0
-    }
-  },
   props: {
-    code: String,
-    value: String,
+    code: {
+      type: String,
+      default: ''
+    },
+    value: {
+      type: String,
+      default: ''
+    },
     readonly: {
       type: Boolean,
       default: () => false
@@ -36,10 +33,12 @@ export default {
           lineNumbers: true,
           lineWrapping: true,
           indentWithTabs: false,
-          highlightSelectionMatches: { minChars: 1 },
-          matchBrackets: true,
+          tabSize: 4,
+          indentUnit: 4,
           autoCloseBrackets: true,
-          styleActiveLine: true
+          styleActiveLine: true,
+          undoDepth: 100,
+          historyEventDelay: 500,
         }
       }
     },
@@ -49,16 +48,106 @@ export default {
         return { sub: true }
       }
     },
-    completions: {
-      type: Function,
-      default: (word) => {}
-    },
     replaceRange: {
       type: Object,
       default: function () {
         return null
       }
     }
+  },
+  data: function () {
+    return {
+      content: '',
+      editor: null,
+      keyMap: { 'Ctrl-Space': 'autocomplete'},
+      changesCounter: 0
+    }
+  },
+  watch: {
+    code (newVal, oldVal) {
+      const editorValue = this.editor.getValue()
+      if (newVal !== editorValue) {
+        let scrollInfo = this.editor.getScrollInfo()
+        this.editor.setValue(newVal)
+        this.content = newVal
+        this.editor.scrollTo(scrollInfo.left, scrollInfo.top)
+      }
+    },
+    value (newVal, oldVal) {
+      const editorValue = this.editor.getValue()
+      if (newVal !== editorValue) {
+        let scrollInfo = this.editor.getScrollInfo()
+        this.editor.setValue(newVal)
+        this.content = newVal
+        this.editor.scrollTo(scrollInfo.left, scrollInfo.top)
+      }
+    },
+    readonly () {
+      this.editor.setOption('readOnly', this.readonly)
+      let codeMirrorEl = document.querySelector('.CodeMirror-wrap', this.$el.parentElement)
+      if (this.readonly) {
+        codeMirrorEl.classList.add('readonly')
+      } else {
+        codeMirrorEl.classList.remove('readonly')
+      }
+    },
+    /**
+     * Replace string from & to a certain range. If `to` is equal to `from` it will just insert a string at that position.
+     * @see https://codemirror.net/doc/manual.html#replaceRange
+     */
+    replaceRange: {
+      handler (newVal, oldVal) {
+        if (newVal && this.editor) {
+          const cursor = this.editor.getCursor()
+          const cursorLineAndCh = {
+            line: cursor.line,
+            ch: cursor.ch
+          }
+          const insert = {
+            ...newVal,
+            from: newVal.from || cursorLineAndCh,
+            to: newVal.to || cursorLineAndCh
+          }
+          this.editor.replaceRange(
+            insert.replacement,
+            insert.from,
+            insert.to
+          )
+        }
+      },
+      deep: true
+    }
+  },
+  created () {
+    // Require language mode config & basic addons.
+    require(`codemirror/mode/swift/swift.js`)
+    require('codemirror/addon/edit/closebrackets.js')
+    require('codemirror/addon/search/match-highlighter.js')
+    require('codemirror/addon/selection/active-line.js')
+    require('codemirror/addon/edit/matchbrackets.js')
+  },
+  mounted () {
+    const options = { ...this.options, lint: this.lintOptions, theme: theme }
+    this.editor = CodeMirror.fromTextArea(this.$el, options)
+    this.editor.setValue(this.code || this.value || this.content)
+    this.editor.addKeyMap(this.keyMap)
+    this.editor.on('change', (cm, changeObj) => {
+      this.updateChangesCounter(changeObj.origin)
+      this.firstLoaded = false
+      this.content = cm.getValue()
+      if (this.$emit) {
+        this.$emit('changed', this.content)
+        this.$emit('input', this.content)
+      }
+    })
+    this.editor.on('cursorActivity', (cm) => {
+      if (this.$emit) {
+        this.$emit('cursor', this.cursorContext(cm))
+      }
+    })
+  },
+  beforeDestroy () { 
+    this.editor.getWrapperElement().remove()
   },
   methods: {
     cursorContext (cm) {
@@ -99,92 +188,6 @@ export default {
       } else {
         this.changesCounter++
       }
-    }
-  },
-  created () {
-    // Require language mode config & basic addons.
-    require(`codemirror/mode/swift/swift.js`)
-    require('codemirror/addon/edit/closebrackets.js')
-    require('codemirror/addon/search/match-highlighter.js')
-    require('codemirror/addon/selection/active-line.js')
-    require('codemirror/addon/edit/matchbrackets.js')
-  },
-  mounted () {
-    const options = { ...this.options, lint: this.lintOptions, theme: theme }
-    this.editor = CodeMirror.fromTextArea(this.$el, options)
-    this.editor.setValue(this.code || this.value || this.content)
-    this.editor.addKeyMap(this.keyMap)
-    this.editor.on('change', (cm, changeObj) => {
-      this.updateChangesCounter(changeObj.origin)
-      this.firstLoaded = false
-      this.content = cm.getValue()
-      if (!!this.$emit) {
-        this.$emit('changed', this.content)
-        this.$emit('input', this.content)
-      }
-    })
-    this.editor.on('cursorActivity', (cm) => {
-      if (!!this.$emit) {
-        this.$emit('cursor', this.cursorContext(cm))
-      }
-    })
-  },
-  beforeDestroy () { 
-    this.editor.getWrapperElement().remove()
-  },
-  watch: {
-    code (newVal, oldVal) {
-      const editorValue = this.editor.getValue()
-      if (newVal !== editorValue) {
-        let scrollInfo = this.editor.getScrollInfo()
-        this.editor.setValue(newVal)
-        this.content = newVal
-        this.editor.scrollTo(scrollInfo.left, scrollInfo.top)
-      }
-    },
-    value (newVal, oldVal) {
-      const editorValue = this.editor.getValue()
-      if (newVal !== editorValue) {
-        let scrollInfo = this.editor.getScrollInfo()
-        this.editor.setValue(newVal)
-        this.content = newVal
-        this.editor.scrollTo(scrollInfo.left, scrollInfo.top)
-      }
-    },
-    readonly () {
-      this.editor.setOption("readOnly", this.readonly)
-      let codeMirrorEl = document.querySelector('.CodeMirror-wrap', this.$el.parentElement)
-      if (this.readonly) {
-        codeMirrorEl.classList.add('readonly')
-      } else {
-        codeMirrorEl.classList.remove('readonly')
-      }
-    },
-    /**
-     * Replace string from & to a certain range. If `to` is equal to `from` it will just insert a string at that position.
-     * @see https://codemirror.net/doc/manual.html#replaceRange
-     */
-    replaceRange: {
-      handler (newVal, oldVal) {
-        if (newVal && this.editor) {
-          const cursor = this.editor.getCursor()
-          const cursorLineAndCh = {
-            line: cursor.line,
-            ch: cursor.ch
-          }
-          const insert = {
-            ...newVal,
-            from: newVal.from || cursorLineAndCh,
-            to: newVal.to || cursorLineAndCh
-          }
-          this.editor.replaceRange(
-            insert.replacement,
-            insert.from,
-            insert.to
-          )
-        }
-      },
-      deep: true
     }
   }
 }
